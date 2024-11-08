64cc86a978549a75f5a511f3d02cbc4a868d9b22
Yomicorne <121363519+Yomicorne@users.noreply.github.com>
2024-11-08 02:47:28 +0000

    only authenticated user can acces the index page

--- project_name/app_name/views.py
+++ project_name/app_name/views.py
@@ -1,19 +1,19 @@
 from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth.decorators import login_required
 from django.db import IntegrityError
-from django.http import HttpResponse, HttpResponseRedirect
+from django.http import HttpResponseRedirect
 from django.shortcuts import render
 from django.urls import reverse
 
 from .models import User
 
-
+# Restrict access to index view for logged-in users only
+@login_required(login_url="login")
 def index(request):
     return render(request, "app_name/index.html")
 
-
 def login_view(request):
     if request.method == "POST":
-
         # Attempt to sign user in
         username = request.POST["username"]
         password = request.POST["password"]
@@ -30,11 +30,9 @@ def login_view(request):
     else:
         return render(request, "app_name/login.html")
 
-
 def logout_view(request):
     logout(request)
     return HttpResponseRedirect(reverse("index"))
-
 
 def register(request):
     if request.method == "POST":
@@ -61,3 +59,71 @@ def register(request):
         return HttpResponseRedirect(reverse("index"))
     else:
         return render(request, "app_name/register.html")
+
+
+
+
+
+# from django.contrib.auth import authenticate, login, logout
+# from django.db import IntegrityError
+# from django.http import HttpResponse, HttpResponseRedirect
+# from django.shortcuts import render
+# from django.urls import reverse
+
+# from .models import User
+
+
+# def index(request):
+#     return render(request, "app_name/index.html")
+
+
+# def login_view(request):
+#     if request.method == "POST":
+
+#         # Attempt to sign user in
+#         username = request.POST["username"]
+#         password = request.POST["password"]
+#         user = authenticate(request, username=username, password=password)
+
+#         # Check if authentication successful
+#         if user is not None:
+#             login(request, user)
+#             return HttpResponseRedirect(reverse("index"))
+#         else:
+#             return render(request, "app_name/login.html", {
+#                 "message": "Invalid username and/or password."
+#             })
+#     else:
+#         return render(request, "app_name/login.html")
+
+
+# def logout_view(request):
+#     logout(request)
+#     return HttpResponseRedirect(reverse("index"))
+
+
+# def register(request):
+#     if request.method == "POST":
+#         username = request.POST["username"]
+#         email = request.POST["email"]
+
+#         # Ensure password matches confirmation
+#         password = request.POST["password"]
+#         confirmation = request.POST["confirmation"]
+#         if password != confirmation:
+#             return render(request, "app_name/register.html", {
+#                 "message": "Passwords must match."
+#             })
+
+#         # Attempt to create new user
+#         try:
+#             user = User.objects.create_user(username, email, password)
+#             user.save()
+#         except IntegrityError:
+#             return render(request, "app_name/register.html", {
+#                 "message": "Username already taken."
+#             })
+#         login(request, user)
+#         return HttpResponseRedirect(reverse("index"))
+#     else:
+#         return render(request, "app_name/register.html")
